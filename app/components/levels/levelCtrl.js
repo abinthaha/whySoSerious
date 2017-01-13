9717cd1d67594566128b8fa4786f998f516e6d73
Hari <Hari>
2017-01-14 00:55:00 +0530

    firebase integration

--- app/components/levels/levelCtrl.js
+++ app/components/levels/levelCtrl.js
@@ -4,7 +4,15 @@
     angular.module('whySoSerious.levels')
         .controller('levelCtrl', levelCtrl);
     /* @ngInject */
-    function levelCtrl() {
-    	
+    function levelCtrl($scope, $firebaseArray ) {
+        var promise = $firebaseArray (firebase.database().ref().child("hackBurst").child("Levels"));
+        $scope.levels = []
+
+        promise.$loaded().then(function() {
+            angular.forEach(promise, function(value) {
+                $scope.levels.push(value);
+                console.log(value)
+            });
+        });
     }
 })();
