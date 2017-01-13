abb730fbbe8b09d6d9e7b1aa5fd6542f7f5880af
Abin Thaha <abin@qburst.com>
2017-01-14 05:08:38 +0530

    Hints issue fixed

--- app/components/levels/questionCtrl.js
+++ app/components/levels/questionCtrl.js
@@ -13,17 +13,21 @@
         $scope.currentQuestion = {};
         $scope.currentIndex = -1;
 
-        if(localStorage.getItem('userData')){
+        if (localStorage.getItem('userData')) {
             $rootScope.user = JSON.parse(localStorage.getItem('userData'));
         }
 
         $http.get('assets/JSON/levels.json').success(function(response) {
-            level = $filter('filter')(response, { id: parseInt($stateParams.levelId) }, true);
-            if (level&&level[0]) {
+            level = $filter('filter')(response, {
+                id: parseInt($stateParams.levelId)
+            }, true);
+            if (level && level[0]) {
                 $http.get('assets/JSON/questions.json').success(function(questions) {
                     angular.forEach(level[0].questions, function(lvl) {
-                        var q = $filter('filter')(questions, { id: parseInt(lvl) }, true);
-                        if (q&&q[0])
+                        var q = $filter('filter')(questions, {
+                            id: parseInt(lvl)
+                        }, true);
+                        if (q && q[0])
                             $scope.questions.push(q[0]);
                     });
                     $scope.getQuestions(0);
@@ -41,26 +45,29 @@
             $scope.currentQuestion = $scope.questions[++$scope.currentIndex];
         }
 
-        $scope.getHint = function(){
-            $rootScope.user.points -= 5;
-            localStorage.setItem('userData',JSON.stringify($rootScope.user));
-            return $scope.currentQuestion.hint;
+        $scope.getHint = function() {
+            if (!$scope.questions[$scope.currentIndex].hintShown) {
+                $rootScope.user.points -= 5;
+                $scope.questions[$scope.currentIndex].hintShown = true;
+                localStorage.setItem('userData', JSON.stringify($rootScope.user));
+                return $scope.currentQuestion.hint;
+            }
         }
 
-        $scope.solve = function(){
+        $scope.solve = function() {
             $rootScope.user.points -= 10;
-            localStorage.setItem('userData',JSON.stringify($rootScope.user));
+            localStorage.setItem('userData', JSON.stringify($rootScope.user));
             $scope.answer = $scope.currentQuestion.answer;
         }
 
-        $scope.checkAnswer = function(){
-            if($scope.answer.toLocaleLowerCase().replace(/ +(?= )/g,'') == $scope.currentQuestion.answer.toLocaleLowerCase().replace(/ +(?= )/g,'')){
+        $scope.checkAnswer = function() {
+            if ($scope.answer.toLocaleLowerCase().replace(/ +(?= )/g, '') == $scope.currentQuestion.answer.toLocaleLowerCase().replace(/ +(?= )/g, '')) {
                 console.log("Solved");
                 $rootScope.user.points += 10;
                 $scope.questions[$scope.currentIndex].disabled = true;
                 $scope.currentQuestion.disabled = true;
-                localStorage.setItem('userData',JSON.stringify($rootScope.user));
-            } else{
+                localStorage.setItem('userData', JSON.stringify($rootScope.user));
+            } else {
                 console.log("ERROR")
             }
         }
