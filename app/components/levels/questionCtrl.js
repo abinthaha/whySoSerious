df236b45bc3c41decc4fb07b7ce478ecf5916c6f
Midhun <midhun@qburst.com>
2017-01-14 00:56:25 +0530

    QUestion showing window

--- app/components/levels/questionCtrl.js
+++ app/components/levels/questionCtrl.js
@@ -5,6 +5,23 @@
         .controller('questionCtrl', questionCtrl);
     /* @ngInject */
     function questionCtrl() {
-    	
+
+    	$scope.questions = [
+    		{
+    			'dialog': 'Why so serious? Let’s put a smile on that face.'
+    			'answer': 'dark knight',
+    			'hint': 'Bat'
+    		},
+    		{
+    			'dialog': 'I’ll make him an offer he can’t refuse..'
+    			'answer': 'The godFather',
+    			'hint': 'father'
+    		},
+    	]
+
+
+    	$scope.showHintsForTheQuestion = function () {
+    		
+    	}
     }
 })();
